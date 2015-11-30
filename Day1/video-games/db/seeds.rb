73a983d3d2a96246ac82b5d4a68cc96b7bdc6588
Roberto Quesada <robertoquesadalizana@gmail.com>
2015-11-30 15:21:12 +0100

    Iteration 1

--- Day1/video-games/db/seeds.rb
+++ Day1/video-games/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+tor1 = Tournament.create(name: 'Tournament 1')
+tor2 = Tournament.create(name: 'Tournament 2')
+
+player1 = Player.create(name: 'Player 1')
+player2 = Player.create(name: 'Player 2')
+player3 = Player.create(name: 'Player 3')
+player4 = Player.create(name: 'Player 4')
+
+
+tor1.players << player1
+tor1.players << player2
+tor1.players << player4
+
+tor2.players << player3
+tor2.players << player2
